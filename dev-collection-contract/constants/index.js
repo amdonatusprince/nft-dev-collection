b051aa859f063bfdb6e8143c2c88e9ec3622e385
amdonatusprince <donatusprince@gmail.com>
2023-05-20 14:12:59 +0100

    v1.0

---
+++ dev-collection-contract/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x188E24768794fA1f126aB97ff5F06D4c5B4bda42";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
